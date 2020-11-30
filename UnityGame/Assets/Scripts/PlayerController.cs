d95117a606730dbc499dba2c86143dc20ca2e811
theowiik <42991003+theowiik@users.noreply.github.com>
2020-11-30 22:55:58 +0100

    Add ability to move

--- UnityGame/Assets/Scripts/PlayerController.cs
+++ UnityGame/Assets/Scripts/PlayerController.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public sealed class PlayerController : MonoBehaviour
 {
-  [SerializeField] private CharacterController characterController;
+  private const            int                 MovementSpeed = 14;
+  [SerializeField] private CharacterController controller;
+
+  private void Update()
+  {
+    var x = Input.GetAxis("Vertical");
+    var y = Input.GetAxis("Horizontal");
+
+    var movementDirNormalized = transform.forward * x + transform.right * y;
+
+    controller.Move(movementDirNormalized * MovementSpeed * Time.deltaTime);
+  }
 }
