85add251a03b42975b1c1db606e9bb5d534e0955
theowiik <42991003+theowiik@users.noreply.github.com>
2021-01-24 02:08:30 +0100

    Create a dotnet format action

--- Game/Assets/Scripts/NonGameObject.cs
+++ Game/Assets/Scripts/NonGameObject.cs
@@ -9,8 +9,8 @@ public sealed class NonGameObject
   /// <param name="number1">First number.</param>
   /// <param name="number2">Second number.</param>
   /// <returns>Returns the sum.</returns>
-  public int Add(int number1, int number2)
-  {
+  public int Add (int number1, int number2)
+    {
     return number1 + number2;
   }
 }
